f59398099ce945c4bbddb79dfbd3bd0d0063df02
diegowh <contacto.diegowh@gmail.com>
2023-10-21 16:17:30 +0200

    Create throttle() and add some comments

--- summoner_profile/api/throttle.py
+++ summoner_profile/api/throttle.py
@@ -1,10 +1,16 @@
 import time
 
-class Throttle():
+class ApiThrottler():
     def __init__(self) -> None:
-        self.BURST_LIMIT = 20
-        self.BURST_TIME = 1
-        self.SUSTAINED_LIMIT = 100
-        self.SUSTAINED_TIME = 120
+        self.BURST_LIMIT = 20 # requests
+        self.BURST_TIME = 1 # seconds
+        self.SUSTAINED_LIMIT = 100 # requests
+        self.SUSTAINED_TIME = 120 # seconds
         
-        self.last_request = 0
+        self.last_request = time.time()
+    
+    def throttle(self) -> None:
+        time_since_last_request = time.time() - self.last_request
+        if time_since_last_request < self.BURST_TIME:
+            time.sleep((self.BURST_TIME - time_since_last_request) + 0.1)
+        self.last_request = time.time()
