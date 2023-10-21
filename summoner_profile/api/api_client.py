a521fee99f663bd94ddcf53670f135dee2d279a1
diegowh <contacto.diegowh@gmail.com>
2023-10-21 19:02:35 +0200

    Create endpoint constants

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -4,6 +4,19 @@ import requests
 from api_throttler import ApiThrottler
 
 class ApiClient:
+    
+    BASE_URL = "https://{server}.api.riotgames.com/"
+    BASE_URL_LOL = BASE_URL + "lol/"
+    BASE_URL_TFT = BASE_URL + "tft/"
+    BASE_URL_VAL = BASE_URL + "val/"
+    BASE_URL_LOR = BASE_URL + "lor/"
+    BASE_URL_RIOT = BASE_URL + "riot/"
+    
+    PLATFORMS = ["br1","eun1","euw1","jp1","kr","la1","la2","na1","oc1","tr1","ru"]
+    REGIONS = ["americas","asia","europe", "esports","ap","br","eu","kr","latam","na"]
+    PLATFORMS_TO_REGIONS = {"br1":"americas","eun1":"europe","euw1":"europe","jp1":"asia","kr":"asia","la1":"americas","la2":"americas","na1":"americas","oc1":"americas","tr1":"europe","ru":"europe"}
+    TOURNAMENT_REGIONS = "americas"
+    
     def __init__(self) -> None:
         self.api_key = os.environ['RIOT_API_KEY']
         self.api_throttler = ApiThrottler()
