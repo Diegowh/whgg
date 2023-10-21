08a21747835534a910508fb87d4bd18463f01087
diegowh <contacto.diegowh@gmail.com>
2023-10-21 12:39:28 +0200

    Change model attribute data type

--- WHGG/summoner_profile/models/item.py
+++ WHGG/summoner_profile/models/item.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Item(models.Model):
-    id = models.CharField(max_length=200, primary_key=True, unique=True)
+    id = models.IntegerField(max_length=200, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
