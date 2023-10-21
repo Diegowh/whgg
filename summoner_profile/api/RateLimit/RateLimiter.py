fbb43503fcbb6edf0712032fadd93ef0fce2bf44
diegowh <contacto.diegowh@gmail.com>
2023-10-21 19:09:47 +0200

    Create RateLimiter

---
+++ summoner_profile/api/RateLimit/RateLimiter.py
@@ -0,0 +1,34 @@
+import asyncio
+import time
+
+class RateLimiter:
+    
+    def __init__(self, debug, limits: (int, int)=(10, 10), name: str = "") -> None:
+        
+        # Initialize Async lock for the rate limiter
+        self.lock = asyncio.Lock()
+        self.back_lock = asyncio.Lock()
+        
+        # Limits params
+        self.limit = limits[0]
+        self.duration = limits[1]
+        
+        # Count and begin of the time window
+        self.count = 0
+        self.time = 0
+        
+        # Name of the limiter for debug purposes
+        self.name = name
+        
+        # "ID" of the time window
+        self.num = 0
+        
+        # Init outgoing requests counters
+        self.currently_pending = 0
+        self.previous_pending = 0
+        
+        # Synchronicity state with server time window
+        self.synced = False
+        
+        # Debug mode
+        self.debug = debug
