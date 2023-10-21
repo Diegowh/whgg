e08c82e42cd455231de18be84f62c42147db7151
diegowh <contacto.diegowh@gmail.com>
2023-10-21 17:16:43 +0200

    Update api_client.py

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -1,5 +1,10 @@
 import os
+import requests
+from api_throttler import ApiThrottler
 
 class ApiClient:
     def __init__(self) -> None:
         self.api_key = os.environ['RIOT_API_KEY']
+        
+    def request(self, url, params):
+        api_throttler = ApiThrottler()
