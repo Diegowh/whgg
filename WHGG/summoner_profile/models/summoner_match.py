dc46ed27e31768b9d4b9b8735420b46020f99692
diegowh <contacto.diegowh@gmail.com>
2023-10-21 12:43:12 +0200

    Add model attributes and FK

--- WHGG/summoner_profile/models/summoner_match.py
+++ WHGG/summoner_profile/models/summoner_match.py
@@ -2,4 +2,15 @@ from django.db import models
 
 class SummonerMatch(models.Model):
     id = models.IntegerField(max_length=200, primary_key=True, unique=True)
+    champion_name = models.CharField(max_length=200)
+    win = models.BooleanField()
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    assists = models.IntegerField()
+    kda = models.FloatField()
+    minion_kills = models.IntegerField()
+    vision_score = models.IntegerField()
+    team_position = models.CharField(max_length=200)
     
+    # Foreign Keys
+    summoner = models.ForeignKey('Summoner', on_delete=models.CASCADE, related_name='summoner_matches')
