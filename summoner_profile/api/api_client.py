0f4da48f0aa27401003bfb8a33a9907df52cdaf6
diegowh <contacto.diegowh@gmail.com>
2023-10-21 17:31:21 +0200

    Create request() method

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -1,10 +1,22 @@
 import os
+import time
 import requests
 from api_throttler import ApiThrottler
 
 class ApiClient:
     def __init__(self) -> None:
         self.api_key = os.environ['RIOT_API_KEY']
+        self.api_throttler = ApiThrottler()
         
     def request(self, url, params):
-        api_throttler = ApiThrottler()
+        self.api_throttler.throttle()
+        
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            if response.status_code == 429:
+                retry_after = int(response.headers.get('Retry-After', 1))
+                print(f'API rate limit exceeded. Retrying in {retry_after} seconds.')
+                time.sleep(retry_after)
+                
