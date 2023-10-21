ea5e2fc70774c4ef9bf2f47ee5c0c64f9404bf9b
diegowh <contacto.diegowh@gmail.com>
2023-10-21 14:22:13 +0200

    Create Participant model

---
+++ WHGG/summoner_profile/models/participant.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Participant(models.Model):
+    id = models.CharField(max_length=200, primary_key=True, unique=True)
+    name = models.CharField(max_length=200)
+    champion_name = models.CharField(max_length=200)
+    team_id = models.IntegerField()
