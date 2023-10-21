d153d78d7bafdbd0db481195e5f28a3730bd6e92
diegowh <contacto.diegowh@gmail.com>
2023-10-21 12:24:36 +0200

    Create ranked_stats model

---
+++ WHGG/summoner_profile/models/ranked_stats.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class RankedStats(models.Model):
+    id = models.CharField(max_length=200, primary_key=True, unique=True)
+    queue_type = models.CharField(max_length=200)
+    rank = models.CharField(max_length=200)
+    league_points = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    winrate = models.IntegerField() # Lo quiero redondeado a un entero
