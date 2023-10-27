3fff49f34255a1f7287a698107c049981a204d07
diegowh <contacto.diegowh@gmail.com>
2023-10-27 19:10:01 +0200

    Delete deprecated commented method

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -193,17 +193,3 @@ class ApiClient:
                     raise e
                 
         return _auto_retry
-    
-    # def request(self, url, params):
-    #     self.api_throttler.throttle()
-        
-    #     try:
-    #         response = requests.get(url, params=params)
-    #         response.raise_for_status()
-    #     except requests.exceptions.RequestException as e:
-    #         if response.status_code == 429:
-    #             retry_after = int(response.headers.get('Retry-After', 1))
-    #             print(f'API rate limit exceeded. Retrying in {retry_after} seconds.')
-    #             print("Are you sure ApiThrottler is working?")
-    #             time.sleep(retry_after)
-                
