5ceadd87764f5137a9be7871879b2f36198ff096
diegowh <contacto.diegowh@gmail.com>
2023-10-27 18:55:47 +0200

    Create get_limits()

--- summoner_profile/api/utils/utils.py
+++ summoner_profile/api/utils/utils.py
@@ -8,3 +8,17 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         
         return cls._instances[cls]
+    
+    
+def getLimits(headers):
+    if 'X-Method-Rate-Limit' in headers and 'X-App-Rate-Limit' in headers:
+        appLimits = {}
+        for appLimit in headers['X-App-Rate-Limit'].split(","):
+            appLimits[int(appLimit.split(":")[1])] = int(appLimit.split(":")[0])
+
+        methodLimits = {}
+        for methodLimit in headers['X-Method-Rate-Limit'].split(","):
+            methodLimits[int(methodLimit.split(":")[1])] = int(methodLimit.split(":")[0])
+
+        return (appLimits,methodLimits)
+    return None
