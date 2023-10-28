b5cf2da8ec6cd93c27c96272677256f775b007f2
diegowh <contacto.diegowh@gmail.com>
2023-10-28 14:51:13 +0200

    Add get_champion_masteries_score

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -298,3 +298,14 @@ class ApiClient:
         Returns the result of https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getChampionMastery
         """
         return await self.fetch((self.BASE_URL_LOL + "champion-mastery/v4/champion-masteries/by-summoner/{summoner_id}/by-champion/{champion_id}").format(server=self._platform, summonerId=summoner_id, championId=champion_id))
+    
+    @auto_retry
+    @exceptions
+    @ratelimit_platform
+    async def get_champion_masteries_score(self, summoner_id):
+        """
+        :param string summonerId: summonerId of the player
+        
+        Returns the result of https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getChampionMasteryScore
+        """
+        return await self.fetch((self.BASE_URL_LOL + "champion-mastery/v4/scores/by-summoner/{summoner_id}").format(server=self._platform, summoner_id=summoner_id))
