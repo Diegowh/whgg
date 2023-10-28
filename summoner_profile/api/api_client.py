36516a850b1f0d2c8efc27b98a0ae051468040e0
diegowh <contacto.diegowh@gmail.com>
2023-10-28 17:02:31 +0200

    Edit type hints

--- summoner_profile/api/api_client.py
+++ summoner_profile/api/api_client.py
@@ -30,7 +30,7 @@ class ApiClient:
     
     SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
     
-    def __init__(self, server, api_key, auto_retry = False, requests_logging_function = None, debug=False) -> None:
+    def __init__(self, server: str, api_key: str, auto_retry: bool = False, requests_logging_function: function = None, debug: bool = False) -> None:
         self._key = api_key
         self._rl = RateLimiterManager(debug)
         
