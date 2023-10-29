3926563ca6f8747cc5bf38d53d524fd0b82663df
diegowh <contacto.diegowh@gmail.com>
2023-10-29 13:10:42 +0100

    [TODO] Refactor RateLimiterServer.get_back()

--- summoner_profile/api/RateLimit/rate_limiter_manager.py
+++ summoner_profile/api/RateLimit/rate_limiter_manager.py
@@ -156,6 +156,7 @@ class RateLimiterServer:
                 return
             
     async def get_back(self, method: str, token, timestamp: int, limits):
+        #TODO Refactor this method to follow single responsibility principle
         if limits is None:
             for i, app_limit in enumerate(self.application):
                 await app_limit.get_back(token[0][i], timestamp)
