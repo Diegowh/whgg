b4e5e33ffe025d7950bf6102a1e360a446ad4758
diegowh <contacto.diegowh@gmail.com>
2023-11-01 17:26:41 +0100

    Rename attributes to fit with the origin names

--- summoner_profile/models/item.py
+++ summoner_profile/models/item.py
@@ -3,7 +3,7 @@ from django.db import models
 class Item(models.Model):
     id = models.IntegerField(max_length=200, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
-    explanation = models.CharField(max_length=200, null=True)
+    plaintext = models.CharField(max_length=200, null=True)
     description = models.TextField()
-    base_gold = models.IntegerField()
-    total_gold = models.IntegerField()
+    gold_base = models.IntegerField()
+    gold_total = models.IntegerField()
