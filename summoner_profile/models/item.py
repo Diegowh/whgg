ab378347e2c95166197fd9dc22ab1a2743d10772
diegowh <contacto.diegowh@gmail.com>
2023-11-01 14:14:51 +0100

    Add description attribute

--- summoner_profile/models/item.py
+++ summoner_profile/models/item.py
@@ -3,3 +3,4 @@ from django.db import models
 class Item(models.Model):
     id = models.IntegerField(max_length=200, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
+    description = models.TextField()
