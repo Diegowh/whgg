cf93416a07ff26a683af8ea345afc55a0e93af55
diegowh <contacto.diegowh@gmail.com>
2023-11-02 17:17:42 +0100

    Update version.py

--- summoner_profile/models/version.py
+++ summoner_profile/models/version.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Version(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
-    name = models.CharField(unique=True)
+    name = models.CharField(unique=True, max_length=50)
