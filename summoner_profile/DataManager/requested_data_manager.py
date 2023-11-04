84f3500cca9df94f77e1f2ac6e9faf31a0c94cc3
diegowh <contacto.diegowh@gmail.com>
2023-11-04 16:14:27 +0100

    Create requested_data_manager.py

---
+++ summoner_profile/DataManager/requested_data_manager.py
@@ -0,0 +1,69 @@
+
+from .dataclasses import (
+    
+    SummonerInfo,
+    SoloqInfo,
+    FlexInfo,
+    RankedInfo,
+    ChampionStats,
+    
+)
+
+class RequestedDataManager:
+    
+    def __init__(self, summoner_name: str, region: str) -> None:
+        
+        self.data = {
+            
+            "summoner_info": {
+                "name": None,
+                "icon_id": None,
+                "account_level": None,
+            },
+            
+            "ranked_info": {
+                
+                "soloq": {
+                    "rank": None,
+                    "league_points": None,
+                    "wins": None,
+                    "losses": None,
+                    "winrate": None,
+                    },
+                    
+                "flex": {
+                    "rank": None,
+                    "league_points": None,
+                    "wins": None,
+                    "losses": None,
+                    "winrate": None,
+                    },
+            },
+            
+            "champion_stats": [
+                {
+                    "name": None,
+                    "games": None,
+                    "winrate": None,
+                    "kda": None,
+                    "kills": None,
+                    "deaths": None,
+                    "assists": None,
+                    
+                },
+            ]
+        }
+    
+    def _add_summoner_info(self, summoner_info: SummonerInfo):
+        # Add SummonerInfo data into self.data.
+        pass
+    
+    def _add_ranked_info(self, ranked_info: RankedInfo):
+        # Add RankedInfo data into self.data
+        pass
+    
+    def _add_champion_stats(self, champion_stats: ChampionStats):
+        
+        all_champion_stats = []
+        
+        all_champion_stats.append(champion_stats)
