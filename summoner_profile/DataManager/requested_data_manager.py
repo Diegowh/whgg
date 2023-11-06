886acdd380e80b00a6f1e2ca43dd0c06749372f6
diegowh <contacto.diegowh@gmail.com>
2023-11-06 23:20:33 +0100

    Small fix

--- summoner_profile/DataManager/requested_data_manager.py
+++ summoner_profile/DataManager/requested_data_manager.py
@@ -80,7 +80,7 @@ class RequestedDataManager:
         
         self.api_client = ApiClient(server=self.server, api_key=api_key, debug=True)
         
-        self._puuid = async_to_sync(self.api_client.get_summoner_by_name)(summoner_name)
+        self._puuid = async_to_sync(self.api_client.get_summoner_by_name)(self.summoner_name)
         
     
     def _add_summoner_info(self, summoner_info: SummonerInfo):
