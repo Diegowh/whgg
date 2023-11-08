127a580422b7fd05cb525f06c35ed767b8e0eef8
diegowh <contacto.diegowh@gmail.com>
2023-11-08 13:11:23 +0100

    Create _update_summoner()

--- summoner_profile/DataManager/db_manager.py
+++ summoner_profile/DataManager/db_manager.py
@@ -9,4 +9,23 @@ from ..models.models import (
 )
 
 class DbManager:
-    pass
+    
+    def __init__(self) -> None:
+        pass
+    
+    def update(self, puuid: str, data: dict):
+        # Here all update methods are called
+        pass
+    
+    def _update_summoner(self, puuid:str, data: dict):
+        
+        defaults = {
+            "id": data["id"],
+            "name": data["name"],
+            "server": data["server"],
+            "icon_id": data["icon_id"],
+            "summoner_level": data["summoner_level"],
+            "last_update": data["last_update"]
+        }
+        
+        Summoner.objects.update_or_create(puuid=puuid, defaults=defaults)
