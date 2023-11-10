a65668fde0845d7a4b09aa457ae8a8f9d423f565
diegowh <contacto.diegowh@gmail.com>
2023-11-10 13:07:24 +0100

    Update class model to include relevant fields

--- summoner_profile/models/summoner_spell.py
+++ summoner_profile/models/summoner_spell.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class SummonerSpell(models.Model):
-    id = models.IntegerField(max_length=200, primary_key=True, unique=True)
+    id = models.CharField(max_length=200, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=200)
+    sprite_name = models.CharField(max_length=200)
