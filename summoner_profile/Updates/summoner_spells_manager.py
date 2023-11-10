9cdd9380dab714b6ebe2d97686834e88e11d4931
diegowh <contacto.diegowh@gmail.com>
2023-11-10 15:29:15 +0100

    [REFACTOR] Now inherits from BaseManager

--- summoner_profile/Updates/summoner_spells_manager.py
+++ summoner_profile/Updates/summoner_spells_manager.py
@@ -1,25 +1,17 @@
 import requests
 
+
+from .base_manager import BaseManager
+
 from ..DataManager.db_manager import DbManager
 
-
-class SummonerSpellsManager:
+class SummonerSpellsManager(BaseManager):
     
     SUMMONER_SPELLS_URL = "https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/summoner.json"
     
     
     def __init__(self) -> None:
-        
-        self._summ_spells = []
-        
-        # Create an instance for DbManager
-        self.db_manager = DbManager()
-        
-    def summ_spells(self) -> list:
-        '''
-        Returns all summoner spells collected data
-        '''
-        return self._summ_spells
+        super().__init__(url=self.SUMMONER_SPELLS_URL)
     
     def _filter(self, json: dict) -> list:
         '''
@@ -43,26 +35,4 @@ class SummonerSpellsManager:
                 
                 summ_spells.append(spell)
                 
-            self._summ_spells = summ_spells
-            
-    def _fetch(self):
-        
-        response = requests.get(self.SUMMONER_SPELLS_URL)
-        summ_spells_json = response.json()
-        
-        if isinstance(summ_spells_json, dict) and len(summ_spells_json) > 0:
-            self._filter(summ_spells_json)
-
-        
-    def update(self):
-        '''
-        Updates self._summ_spells to the latest values collecting the data from DataDragon'''
-        try:
-            self._fetch()
-        
-        except Exception as e:
-            print(e)
-            return None
-        
-        # Save the new summoner spells data into the database
-        self.db_manager.update_summoner_spells(summoner_spells=self.summ_spells())
+            self._data = summ_spells
