ff59c175d0cf9e82d38c737a8e7437e2fdf3b634
diegowh <contacto.diegowh@gmail.com>
2023-11-10 15:12:40 +0100

    Refactor inheritance. Set update() as an abstractmethod

--- summoner_profile/Updates/base_manager.py
+++ summoner_profile/Updates/base_manager.py
@@ -5,11 +5,11 @@ from ..DataManager.db_manager import DbManager
 
 class BaseManager(ABC):
 
-    def __init__(self, url, update_func) -> None:
+    def __init__(self, url, db_manager, update_func) -> None:
         
         self._data = []
         self.url = url
-        self.db_manager = DbManager()
+        self.db_manager = db_manager
         self.update_func = update_func
         
     @property
@@ -19,19 +19,19 @@ class BaseManager(ABC):
     @abstractmethod
     def _filter(self, json: dict) -> None:
         pass
+        
+    @abstractmethod
+    def update(self):
+        pass
     
     def _fetch(self):
-        response = requests.get(self.url)
-        data_json = response.json()
-        
-        if isinstance(data_json, dict) and len(data_json) > 0:
-            self._filter(data_json)
-            
-    def update(self):
         try:
-            self._fetch()
+            response = requests.get(self.url)
+            data_json = response.json()
+        
+            if isinstance(data_json, dict) and len(data_json) > 0:
+                self._filter(data_json)
+                
         except Exception as e:
             print(e)
             return None
-        
-        self.update_func(self.data)
