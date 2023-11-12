04b1f3865995bbe66298b7f5b246d6475b94ba6b
diegowh <contacto.diegowh@gmail.com>
2023-11-12 14:59:29 +0100

    Set imports on __init__ instead of models.py

--- summoner_profile/models/models.py
+++
@@ -1,7 +0,0 @@
-from ..models.champion_stats import ChampionStats
-from ..models.item import Item
-from ..models.participant import Participant
-from ..models.ranked_stats import RankedStats
-from ..models.summoner_match import SummonerMatch
-from ..models.summoner import Summoner
-from ..models.summoner_spell import SummonerSpell
