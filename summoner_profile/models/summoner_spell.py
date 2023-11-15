6c96d2e8ecafc94c084781715f29eb2e7cc367e8
diegowh <contacto.diegowh@gmail.com>
2023-11-15 22:00:23 +0100

    Change data type

--- summoner_profile/models/summoner_spell.py
+++ summoner_profile/models/summoner_spell.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class SummonerSpell(models.Model):
-    id = models.CharField(max_length=200, primary_key=True, unique=True)
+    id = models.IntegerField(max_length=200, primary_key=True, unique=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=300)
     image_name = models.CharField(max_length=200)
