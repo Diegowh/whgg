deeb13292bbdb3f15a3cd053bece34d8921102bd
diegowh <contacto.diegowh@gmail.com>
2023-11-15 21:49:11 +0100

    Rename attribute

--- summoner_profile/models/summoner_match.py
+++ summoner_profile/models/summoner_match.py
@@ -6,7 +6,7 @@ class SummonerMatch(models.Model):
     queue_id = models.IntegerField() # https://static.developer.riotgames.com/docs/lol/queues.json
     game_mode = models.CharField() # https://static.developer.riotgames.com/docs/lol/gameModes.json
     game_type = models.CharField() # https://static.developer.riotgames.com/docs/lol/gameTypes.json
-    champion_name = models.CharField(max_length=200)
+    champion_played = models.CharField(max_length=200)
     win = models.BooleanField()
     kills = models.IntegerField()   
     deaths = models.IntegerField()
