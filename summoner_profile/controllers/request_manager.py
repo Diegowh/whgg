9d7cc923ce7c184fed9e2f3e7056280a681f4b02
diegowh <contacto.diegowh@gmail.com>
2023-11-16 13:09:00 +0100

    Remove TODO

--- summoner_profile/controllers/request_manager.py
+++ summoner_profile/controllers/request_manager.py
@@ -88,7 +88,7 @@ class RequestManager:
         return self._server
     
     
-    def fetch_requested_data(self): #TODO Refactorizar este metodo para que esta funcionalidad se haga en la clase DataFormatter. Esta clase debe enviar una peticion de los datos ya formateados a la base de datos mediante el DataFormatter.
+    def fetch_requested_data(self): 
         
         # If the summoner puuid is already in the database
         if self.db_manager.is_puuid_in_database():
