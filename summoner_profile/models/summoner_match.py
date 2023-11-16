ca4f55952fe0430faa85b47376ba1aa1471e2bc1
diegowh <contacto.diegowh@gmail.com>
2023-11-16 13:29:00 +0100

    Update summoner_match.py

--- summoner_profile/models/summoner_match.py
+++ summoner_profile/models/summoner_match.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class SummonerMatch(models.Model):
     id = models.CharField(max_length=200, primary_key=True, unique=True)
-    season_id = models.IntegerField() # https://static.developer.riotgames.com/docs/lol/seasons.json
-    queue_id = models.IntegerField() # https://static.developer.riotgames.com/docs/lol/queues.json
-    game_mode = models.CharField() # https://static.developer.riotgames.com/docs/lol/gameModes.json
-    game_type = models.CharField() # https://static.developer.riotgames.com/docs/lol/gameTypes.json
+    game_start = models.IntegerField()
+    game_end = models.IntegerField()
+    game_duration = models.IntegerField()
+    game_mode = models.CharField()
+    game_type = models.CharField()
     champion_played = models.CharField(max_length=200)
     win = models.BooleanField()
     kills = models.IntegerField()   
