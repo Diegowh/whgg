47e5cbf02d7a3559474e2de9c5bce487a472e490
diegowh <contacto.diegowh@gmail.com>
2023-11-17 12:40:37 +0100

    Create default views

---
+++ summoner_profile/views.py
@@ -0,0 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from controllers.request_manager import RequestManager
+
+
+class SummonerDataView(APIView):
+    
+    def get(self, request, summoner_name: str, server: str, format=None):
+        
+        request_manager = RequestManager(summoner_name=summoner_name, server=server)
+        data = request_manager.requested_data["summoner_info"]
+        return Response(data)
+        
+
+class RankedDataView(APIView):
+    
+    def get(self, request, summoner_name: str, server: str, format=None):
+        
+        request_manager = RequestManager(summoner_name=summoner_name, server=server)
+        data = request_manager.requested_data["ranked_data"]
+        return Response(data)
+    
+    
+class ChampionStatView(APIView):
+    
+    def get(self, request, summoner_name: str, server: str, format=None):
+        
+        request_manager = RequestManager(summoner_name=summoner_name, server=server)
+        data = request_manager.requested_data["champion_stats"]
+        return Response(data)
+    
+class MatchHistoryView(APIView):
+    
+    def get(self, request, summoner_name: str, server: str, format=None):
+        
+        request_manager = RequestManager(summoner_name=summoner_name, server=server)
+        data = request_manager.requested_data["match_history"]
+        return Response(data)
