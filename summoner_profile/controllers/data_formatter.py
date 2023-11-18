1a2c679f2bac5ea9f230d99087a7e72e158d2900
diegowh <contacto.diegowh@gmail.com>
2023-11-18 18:33:57 +0100

    Create boilerplate filter()

--- summoner_profile/controllers/data_formatter.py
+++ summoner_profile/controllers/data_formatter.py
@@ -1,6 +1,20 @@
+from mimetypes import init
 from .db_manager import DbManager
 
 
 class DataFormatter:
     
-    ...
+    def __init__(self) -> None:
+        pass
+    
+    
+    def filter(self, data: dict, type: str):
+        
+        match type:
+            case "summoner":
+                self.filter_summoner(data=data)
+            case "ranked":
+                self.filter_ranked(data=data)
+            case "match":
+                self.filter_match(data=data)
+                
