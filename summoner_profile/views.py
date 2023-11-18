e111d253c60b9e1098f1e224c121c2ca092bafc9
diegowh <contacto.diegowh@gmail.com>
2023-11-18 14:30:03 +0100

    Update views.py

--- summoner_profile/views.py
+++ summoner_profile/views.py
@@ -3,25 +3,10 @@ from rest_framework.response import Response
 from controllers.request_manager import RequestManager
 
 
-class BaseDataView(APIView):
-    data_key = ""
+class ProfileDataView(APIView):
     
     def get(self, request, summoner_name: str, server: str, format=None):
         
         request_manager = RequestManager(summoner_name=summoner_name, server=server)
-        data = request_manager.requested_data[self.data_key]
+        data = request_manager.profile_data
         return Response(data)
-
-class SummonerDataView(APIView):
-    data_key = "summoner_data"
-
-class RankedDataView(APIView):
-    data_key = "ranked_data"
-    
-    
-class ChampionStatView(APIView):
-    data_key = "champion_stats"
-
-    
-class MatchHistoryView(APIView):
-    data_key = "match_history"
