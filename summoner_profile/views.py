994a1b05782a27954b039ec7363bb1ddb1153ac6
diegowh <contacto.diegowh@gmail.com>
2023-11-18 14:39:26 +0100

    Remove format argument

--- summoner_profile/views.py
+++ summoner_profile/views.py
@@ -5,7 +5,7 @@ from controllers.request_manager import RequestManager
 
 class ProfileDataView(APIView):
     
-    def get(self, request, summoner_name: str, server: str, format=None):
+    def get(self, request, summoner_name: str, server: str):
         
         request_manager = RequestManager(summoner_name=summoner_name, server=server)
         data = request_manager.profile_data
