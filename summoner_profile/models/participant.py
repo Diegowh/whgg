54011901e228b88c299f9074d23a70c11774405e
diegowh <contacto.diegowh@gmail.com>
2023-11-20 18:18:28 +0100

    Update participant.py

--- summoner_profile/models/participant.py
+++ summoner_profile/models/participant.py
@@ -1,10 +1,13 @@
+from enum import unique
 from django.db import models
 
 class Participant(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
     puuid = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     champion_name = models.CharField(max_length=200)
     team_id = models.IntegerField()
     
     match = models.ForeignKey('Match', on_delete=models.CASCADE, related_name='participants')
+    
+    class Meta:
+        unique_together = ("puuid", "match")
