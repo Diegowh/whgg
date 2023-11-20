2d90daf7381a324f9d337a5d215c8d130c5b3def
diegowh <contacto.diegowh@gmail.com>
2023-11-20 12:18:02 +0100

    Remove server attribute

--- summoner_profile/models/summoner.py
+++ summoner_profile/models/summoner.py
@@ -4,7 +4,6 @@ class Summoner(models.Model):
     puuid = models.CharField(max_length=200, primary_key=True, unique=True)
     id = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    server = models.CharField(max_length=200)
     icon_id = models.IntegerField()
     summoner_level = models.IntegerField()
     last_update = models.IntegerField()
