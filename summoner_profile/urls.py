4015dbc179e0ed00262e0360586aa4f591c9592f
diegowh <contacto.diegowh@gmail.com>
2023-11-22 01:05:28 +0100

    Remove deprecated urls

--- summoner_profile/urls.py
+++ summoner_profile/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include, re_path
 
-from summoner_profile.apis.backend.views import test_api_client
-from summoner_profile.views import ProfileDataView
 
 urlpatterns = [
     
-    path('test-api-client/', test_api_client, name='test_api_client'),
-    path('<str:server>/<str:summoner_name>/', ProfileDataView.as_view(), name='profile_data'),
+    # path('test-api-client/', test_api_client, name='test_api_client'),
+    # path('<str:server>/<str:summoner_name>/', ProfileDataView.as_view(), name='profile_data'),
     
 ]
