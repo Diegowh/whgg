92157793a4a10b69018b7eb8c9a62945b9e57776
diegowh <contacto.diegowh@gmail.com>
2023-11-30 17:12:35 +0100

    Set main view

--- summoner_profile/views.py
+++ summoner_profile/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from summoner_profile.controllers.request_manager import RequestManager
+from .controllers.request_manager import RequestManager
 
 
-class ProfileDataView(APIView):
+class SummonerProfileView(APIView):
     
     def get(self, request, summoner_name: str, server: str):
         
         request_manager = RequestManager(summoner_name=summoner_name, server=server)
-        data = request_manager.get()
-        return Response(data)
+        response_data = request_manager.get()
+        return Response(response_data.to_dict())
