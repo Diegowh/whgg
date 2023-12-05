09f441743f7ea20a197c6055c79824d00fb451f5
diegowh <contacto.diegowh@gmail.com>
2023-12-05 17:18:18 +0100

    Update updater.py

--- summoner_profile/Updates/updater.py
+++ summoner_profile/Updates/updater.py
@@ -1,7 +1,9 @@
+from sys import version
 import requests
 
 
 from .items_manager import ItemsManager
+from .summoner_spells_manager import SummonerSpellsManager
 from ..models.version import Version
 
 from ..controllers.db_manager import DbManager
@@ -20,16 +22,56 @@ class Updater:
         
         self.db_manager = DbManager()
         
-        self.latest_version = None
-        self.previous_version = None
+        self._previous_version = None
+        self._latest_version = None
         
-        # Checks if there is a previous version on the database
+    
+    # Getters
+    @property
+    def previous_version(self):
+        return self._previous_version
+        
+    @property
+    def latest_version(self):
+        return self._latest_version
+    
+    # Setters
+    @previous_version.setter
+    def previous_version(self, version: str):
+        self._previous_version = version
+    
+    @latest_version.setter
+    def latest_version(self, version: str):
+        self._latest_version = version
+        
+    # Methods        
+    def update(self):
+        
+        # Setear la version previa
         version_query = Version.objects.first()
         if version_query:
             self.previous_version = version_query.version
-            self.latest_version = self.previous_version
+            
+        # Obtenemos la ultima version
+        self.check_for_updates()
+        
+        if not self.is_updated():
+            self._save_latest_version()
+            print("No hay actualizaciones disponibles")
+            return None
+        
+        # Actualizamos los items
+        items_manager = ItemsManager()
+        items = items_manager.fetch()
+        self.db_manager.update_items(items)
+        
+        # Actualizamos los Summoner Spells
+        summ_spell_manager = SummonerSpellsManager()
+        summ_spells = summ_spell_manager.fetch()
+        self.db_manager.update_summoner_spells(summ_spells)
         
         
+            
     def check_for_updates(self):
         
         try:
@@ -42,11 +84,7 @@ class Updater:
             return None
             
         if isinstance(versions_json, list) and len(versions_json) > 0:
-            
             self.latest_version = versions_json[0]
-            
-            # Saves it on the database
-            self._save_latest_version()
             
         else:
             print("An error ocurred trying to validate versions_json type or length")
@@ -63,18 +101,27 @@ class Updater:
         '''
         Saves self.latest_version into the database.
         '''
-        Version.objects.update_or_create(
-            defaults= {
+        version_obj, created = Version.objects.get_or_create(
+            version=self.latest_version,
+            defaults={
                 'version': self.latest_version,
             }
         )
+        if not created:
+            version_obj.version = self.latest_version
+            version_obj.save()
+            
+    def force_update(self):
+        '''
+        Forces the update of the database.
+        '''
         
-    def update_items(self):
-        
+        # Actualizamos los items
         items_manager = ItemsManager()
+        items = items_manager.fetch()
+        self.db_manager.update_items(items)
         
-        if items_manager.is_updated():
-            
-            items = items_manager.data
-            
-            self.db_manager.update_items(items=items)
+        # Actualizamos los Summoner Spells
+        summ_spell_manager = SummonerSpellsManager()
+        summ_spells = summ_spell_manager.fetch()
+        self.db_manager.update_summoner_spells(summ_spells)
