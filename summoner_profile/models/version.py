45cc14d5be40256c32e724ae7de3a3648cda5590
diegowh <contacto.diegowh@gmail.com>
2023-12-05 17:19:39 +0100

    Update version.py

--- summoner_profile/models/version.py
+++ summoner_profile/models/version.py
@@ -1,4 +1,8 @@
+import time
 from django.db import models
 
 class Version(models.Model):
-    ...
+    version = models.CharField(max_length=200, primary_key=True, unique=True, default='0.0.0')
+    
+    def __str__(self):
+        return self.version
