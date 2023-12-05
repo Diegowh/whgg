21623d5abad0426e184ee9739732d5f37b6ece8e
diegowh <contacto.diegowh@gmail.com>
2023-12-05 17:17:45 +0100

    Add commands for periodic db updates

---
+++ summoner_profile/management/commands/periodic_update.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from summoner_profile.updates.updater import Updater
+
+class Command(BaseCommand):
+    help = "Updates the database with the latest items and summoner spells"
+    
+    def handle(self, *args, **options):
+        updater = Updater()
+        updater.update()
+        
+        self.stdout.write(self.style.SUCCESS('Database updated successfully'))
