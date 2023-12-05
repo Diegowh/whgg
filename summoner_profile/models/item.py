e84802f91ccc305fc0accb602de29f6574361569
diegowh <contacto.diegowh@gmail.com>
2023-12-05 17:20:42 +0100

    Make plaintext a TextField

--- summoner_profile/models/item.py
+++ summoner_profile/models/item.py
@@ -3,7 +3,7 @@ from django.db import models
 class Item(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=200)
-    plaintext = models.CharField(max_length=200, null=True)
+    plaintext = models.TextField(null=True)
     description = models.TextField()
     gold_base = models.IntegerField()
     gold_total = models.IntegerField()
