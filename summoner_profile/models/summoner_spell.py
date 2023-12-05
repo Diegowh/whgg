b5ed25597730746b27e6391862e0adc87b85530e
diegowh <contacto.diegowh@gmail.com>
2023-12-05 17:20:03 +0100

    Alter IntegerField into CharField

--- summoner_profile/models/summoner_spell.py
+++ summoner_profile/models/summoner_spell.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class SummonerSpell(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    id = models.CharField(max_length=200,primary_key=True, unique=True)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=300)
+    key = models.IntegerField(default=0)
+    description = models.TextField(max_length=300)
     image_name = models.CharField(max_length=200)
     sprite_name = models.CharField(max_length=200)
